b576975b401fff7b129b0b3e42ee0aa9aa2e66d7
Nikhil Prasad <nprasad2077@gmail.com>
2026-06-19 21:42:54 -0500

    Update date range for box score data import

--- import.go
+++ import.go
@@ -80,7 +80,7 @@ func importGameSchedules(db *gorm.DB) {
 // for games within a recent date range.
 func importBoxScores(db *gorm.DB) {
 	from := time.Date(2010, time.November, 4, 0, 0, 0, 0, time.UTC)
-	to := time.Date(2010, time.November, 24, 5, 30, 0, 0, time.UTC)
+	to := time.Date(2010, time.November, 14, 5, 30, 0, 0, time.UTC)
 
 	dateRangeComment := fmt.Sprintf("--- Starting Box Score Data Import for games between %s and %s ---",
 		from.Format("January 2, 2006"),
